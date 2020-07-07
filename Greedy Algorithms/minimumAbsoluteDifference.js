c906970384e8bc70d4d7f71f620d5cc33124ea7b
Max Hardy <max@hardy.work>
2020-07-07 12:51:26 +0100

    luckBalance and minAbsDiff added + tests

---
+++ Greedy Algorithms/minimumAbsoluteDifference.js
@@ -0,0 +1,18 @@
+/**
+ * Minimum Absolute Difference in an Array
+ * https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+ * 
+ * Difficulty: Easy
+ * @param {number[]} arr: an array of integers
+ */
+function minimumAbsoluteDifference(arr) {
+    const n = arr.length;
+    const subtract = [];
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < n - 1; i++) {
+        subtract.push(Math.abs(Number(arr[i]) - Number(arr[i + 1])));
+    }
+    return Math.min(...subtract);
+}
+
+module.exports = minimumAbsoluteDifference;
---
+++ __tests__/luckBalance.spec.js
@@ -0,0 +1,30 @@
+const luckBalance = require('../Greedy Algorithms/luckBalance');
+
+describe('luckBalance function', () => {
+    test('it should return 10 if k = 2 and contests = [[5, 1], [1, 1], [4, 0]]', () => {
+        const k = 2;
+        const contests = [[5, 1], [1, 1], [4, 0]];
+        const input = [k, contests];
+        const output = 10;
+
+        expect(luckBalance(...input)).toEqual(output);
+    })
+
+    test('it should return 8 if k = 1 and contests = [[5, 1], [1, 1], [4, 0]]', () => {
+        const k = 1;
+        const contests = [[5, 1], [1, 1], [4, 0]];
+        const input = [k, contests];
+        const output = 8;
+
+        expect(luckBalance(...input)).toEqual(output);
+    })
+
+    test('it should return 29 if k = 3 and contests = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]', () => {
+        const k = 3;
+        const contests = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]];
+        const input = [k, contests];
+        const output = 29;
+
+        expect(luckBalance(...input)).toEqual(output);
+    })
+})
